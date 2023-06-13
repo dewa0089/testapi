34c53401083b82fd88332affb3b46225592f6822
dewa0089 <newdewa03@gmail.com>
2023-06-13 18:05:26 +0700

    coba3

--- routes/products.js
+++ routes/products.js
@@ -36,9 +36,9 @@ router.get('/', async(req, res) => {
 
 
 //Get id
-router.get('/:kategori', async (req, res) => {
+router.get('/productskategori', async (req, res) => {
     try{
-        const products = await Products.findById(req.params.kategori);
+        const products = await Products.findById(req.params.productskategori);
         res.json(products)
     }
     catch(error){

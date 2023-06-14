ddd3194d7ed1b2b11622ab23eadd8c70ae0a052d
dewa0089 <newdewa03@gmail.com>
2023-06-14 14:17:07 +0700

    update

--- models/Keranjang.js
+++ models/Keranjang.js
@@ -3,7 +3,34 @@ const mongoose = require('mongoose')
 
 const KeranjangSchema = mongoose.Schema({
     // Buat Schema data
-    
+    jumlah_pemesanan:{
+        type: String,
+        required : true
+    },
+    keterangan:{
+        type: String,
+        required : true
+    },
+    best:{
+        type: String,
+        required : true
+    },
+    gambar:{
+        type: String,
+        required : true
+    },
+    harga:{
+        type: String,
+        required : true
+    },
+    kategori:{
+        type: String,
+        required : true
+    },
+    nama:{
+        type: String,
+        required : true
+    },
     createdAt: {
         type: Date,
         default: Date.now

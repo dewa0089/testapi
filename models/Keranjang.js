4e880ff0c57b52f866727c072e37dc1377ddb22b
dewa0089 <newdewa03@gmail.com>
2023-06-16 13:01:22 +0700

    post

--- models/Keranjang.js
+++ models/Keranjang.js
@@ -3,38 +3,7 @@ const mongoose = require('mongoose')
 
 const KeranjangSchema = mongoose.Schema({
     // Buat Schema data
-    jumlah_pemesanan:{
-        type: String,
-        required : true
-    },
-    keterangan:{
-        type: String,
-        required : true
-    },
-    nama: {
-        type: String,
-        required: true
-    },
-    harga: {
-        type: Number,
-        required: true
-    },
-    gambar: {
-        type: String,
-        required: true
-    },
-    best: {
-        type: Number,
-        required: true
-    },
-    ketegori: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+
 })
 
 module.exports = mongoose.model('Keranjang', KeranjangSchema)
--- routes/keranjang.js
+++ routes/keranjang.js
@@ -4,27 +4,40 @@ const router = express.Router()
 const Keranjang = require('../models/Keranjang')
 
 // Create 
-router.post('/', async(req, res) => {
-    // tampung input mahasiswa 
-    const keranjangPost = new Keranjang({
-        jumlah_pemesanan: req.body.jumlah_pemesanan,
-        keterangan: req.body.keterangan,
-        nama: req.body.nama,
-        harga: req.body.harga,
-        gambar: req.body.gambar,
-        best: req.body.best,
-        kategori: req.body.kategori
-    })
+// router.post('/', async(req, res) => {
+//     // tampung input mahasiswa 
+//     const keranjangPost = new Keranjang({
+//         jumlah_pemesanan: req.body.jumlah_pemesanan,
+//         keterangan: req.body.keterangan,
+//         nama: req.body.nama,
+//         harga: req.body.harga,
+//         gambar: req.body.gambar,
+//         best: req.body.best,
+//         kategori: req.body.kategori
+//     })
 
-    try {
-        // simpan data 
-        const keranjang = await keranjangPost.save()
-        // response
-        res.json(keranjang)
-    } catch (error) {
-        res.json({message: error})
-    }
-})
+//     try {
+//         // simpan data 
+//         const keranjang = await keranjangPost.save()
+//         // response
+//         res.json(keranjang)
+//     } catch (error) {
+//         res.json({message: error})
+//     }
+// })
+
+
+function post(req, res, next) {
+    const prop = pluralize.singular(req.params.resource)
+    req.body[`${prop}${opts.foreignKeySuffix}`] = req.params.id
+    req.url = `/${req.params.nested}`
+    next()
+
+    return router
+    .post('/:resource/:id/:nested', post)
+  }
+
+  
 
 // Read
 router.get('/', async(req, res) => {
@@ -65,5 +78,7 @@ router.delete('/:keranjangId', async(req, res) => {
         res.json({message: error})
     }
 })
+
+
 
 module.exports = router

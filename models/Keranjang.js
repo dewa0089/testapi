78e268c67fbb5a88819da7d99fd8f11f3c7314e1
dewa0089 <newdewa03@gmail.com>
2023-06-16 12:34:16 +0700

    push

--- models/Keranjang.js
+++ models/Keranjang.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 
 const KeranjangSchema = mongoose.Schema({
     // Buat Schema data
-   
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 })
 
 module.exports = mongoose.model('Keranjang', KeranjangSchema)

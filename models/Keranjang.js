6f3edce904cc6a411e048e88a5928ddb29e78c08
dewa0089 <newdewa03@gmail.com>
2023-06-16 12:01:24 +0700

    push2

--- models/Keranjang.js
+++ models/Keranjang.js
@@ -3,24 +3,7 @@ const mongoose = require('mongoose')
 
 const KeranjangSchema = mongoose.Schema({
     // Buat Schema data
-    jumlah_pemesanan:{
-        type: String,
-        required : true
-    },
-    keterangan:{
-        type: String,
-        required : true
-    },
-    products:{
-        best,
-        createdAt,
-        gambar,
-        harga,
-        kategori,
-        nama,
-        __v,
-        _id
-    },
+   
 })
 
 module.exports = mongoose.model('Keranjang', KeranjangSchema)

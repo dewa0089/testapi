313d43a48c80712f2a4a1a4ebe9ea5a0cae4d783
dewa0089 <newdewa03@gmail.com>
2023-06-17 16:36:45 +0700

    push

--- routes/products.js
+++ routes/products.js
@@ -68,7 +68,7 @@ router.get('/:best/b', async (req, res) => {
 })
 
 //Get id
-router.get('/:nama/q', async (req, res) => {
+router.get('/q/:nama', async (req, res) => {
     try{
         const products = await Products.find({nama:req.query.nama});
         res.json(products)
